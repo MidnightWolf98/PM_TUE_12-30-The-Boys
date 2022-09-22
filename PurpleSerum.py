647c79459cbb8b44ff5946906a4edb1822ac4e92
HunterH25 <101787135+HunterH25@users.noreply.github.com>
2022-09-22 16:41:36 +1000

    Add files via upload

--- PurpleSerum.py
+++ PurpleSerum.py
@@ -11,12 +11,13 @@ def escape(): #define function called escape
     print('2 - Run to rooftop through the stairs.')
     try:
         escape = int(input('Enter 1 or 2:')) #ask user to input an integer
-        if escape != 2: #if input is not 2 run the code below
+        while escape != 2: #while input is not 2 run the code below
             print('You break the window and look..50 floors above the ground, that is not a great choice to jump.')
             print('Try something else.')
             escape = int(input('Enter 1 or 2:'))#ask user to input an integer
-            if escape == 2:
+            if escape == 2: #run code below if input equal to 2
                 escape_2()
+                break #exit while loop
         else: #if input is 2 run the code below
             print('Sounds great to try.')
             escape_2() #call escape_2 function
@@ -45,43 +46,44 @@ escape() #call escape function
 
 
 #scene 3
+print('- - - -- - - - - ')
 print('2 WEEKS LATER')
-print('You are very very tired and going to have a nice bath before bed.')
+print('You come home after work very very tired and going to have a nice bath before bed.')
 print('You get a phone call from your best friend Will.')
 
-call_time = 0
+call_time = 0 #global variable called call_time
 
 def phone_call():
     print('1 - Pick up the call')
     print('2 - Hang up the call')
     try:
-        call = int(input('Enter 1 or 2: '))
-        if call == 1:
+        call = int(input('Enter 1 or 2: ')) #ask user to input an integer
+        if call == 1: #run code below if input is 1
+            print('You pick up the phone,')
             call_1()
-        else:
-            global call_time
-            while call_time < 2:
-                call_time = call_time + 1
-                call_2()
-                if call_time == 2:
-                    call_3()
-                    break 
+        else: #run code below if input is not 1
+            global call_time #get value of global variable  call_time
+            while call_time < 2: #run code below while call_time smaller than 2
+                call_time = call_time + 1 #add 1 to call_time
+                call_2() #call function call_2
+                if call_time == 2: #run code below if call_time equal to 2
+                    call_3() #call function call_3
+                    break #exit while loop
 
     except: #recall function if input is invaild
         print('input invaild!\nEnter 1 or 2 only')
         phone_call()
 
 def call_1():
-    print('You pick up the phone,')
     print('Will: "How are you mate？"\n "There is a party tomorrow nigth, you comming?"')
     print('1 - "Yeah, why not."')
     print('2 - "Let me think about it"')
     print('3 - "Cannot be bother, i wanna stay home."')
     try:
-        call_1 = int(input('Enter 1, 2 or 3'))
-        if call_1 == 1:
+        call_1 = int(input('Enter 1, 2 or 3')) #ask user to input an integer
+        if call_1 == 1: #run code below if input is 1
             print('Will: "Cool! See you then!"')
-        else:
+        else: #run code below when input is not 1
             print('Will: "Come on"\n"We did not go to any party for a whole month."\n"You must be there tomorrow. See you there mate."')
     except: #recall function if input is invaild
         print('input invaild!\nEnter 1 or 2 only')
@@ -92,13 +94,26 @@ def call_2():
     print('2 MINUTES LATER..\nWill call you again.')
     print('He might have something needs to say.')
     phone_call()
-    global call_time
-    if call_time >= 5:
+    global call_time #get value of global variable call_time
+    if call_time >= 5: #run code below if call_time equal to 5
         call_3()
 
 def call_3():
     print('20 MINUTES LATER..')
     print('"KNOCK KNOCK"\n"Are you there? Open the door."')
-    print('It is Will')
-    input()
-phone_call()
+    print('It is Will\nYou opend the door and let him in.')
+    print('Will: "What were you doing? I was worrying so I came."')
+    call_1() #call function call_1
+
+
+phone_call() #call phone_call function
+
+
+#scene 4
+print('- - - -- - - - - ')
+print('NEXT DAY 7PM')
+print('')
+
+
+
+input()
